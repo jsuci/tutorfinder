64e93af092d1fd1b93b1ded313f4708606c6b2d5
jsuci <jsuci.jsuci@gmail.com>
2022-05-09 01:39:43 +0800

    fix overall project structure

---
+++ apps/core/forms.py
@@ -0,0 +1,21 @@
+from allauth.account.forms import LoginForm, SignupForm
+
+class CustomLoginForm(LoginForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CustomLoginForm, self).__init__(*args, **kwargs)
+
+        for fieldname, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'input mb-3'
+            })
+
+class CustomSignUpForm(SignupForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CustomSignUpForm, self).__init__(*args, **kwargs)
+
+        for fieldname, field in self.fields.items():
+            field.widget.attrs.update({
+                'class': 'input mb-3'
+            })
--- tutor/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from tutor import views
-
-
-urlpatterns = [
-    path('list-tutors/', views.AllTutorsList.as_view()),
-    path('list-categories/', views.AllCategoriesList.as_view())
-]
