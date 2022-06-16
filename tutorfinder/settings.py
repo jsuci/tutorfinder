aa4c9c8cc36b71f96d9b9f1faf6655652abdc6f3
jsuci <jsuci.jsuci@gmail.com>
2022-06-17 05:38:50 +0800

    fix: remove pagination

--- tutorfinder/settings.py
+++ tutorfinder/settings.py
@@ -200,7 +200,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 # Rest-framework config
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
-}
+# REST_FRAMEWORK = {
+#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+#     'PAGE_SIZE': 5
+# }
