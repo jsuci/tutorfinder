9fe10e5c324e3807dbd7fd4ba14e60d5429f48ca
jsuci <jsuci.jsuci@gmail.com>
2022-06-17 03:27:53 +0800

    fix: add slash at the end of url not redirecting

--- tutorfinder/urls.py
+++ tutorfinder/urls.py
@@ -1,5 +1,3 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -20,8 +18,3 @@ urlpatterns = [
     path('search/', search, name='search')
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
